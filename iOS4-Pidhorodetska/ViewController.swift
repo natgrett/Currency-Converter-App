e68842c317e9801b867a08dbb75d78ce3ba424bd
Nataliia Pidhorodetska <natpid1989@gmail.com>
2019-11-26 16:55:26 +0100

    Initial Commit

---
+++ iOS4-Pidhorodetska/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS4-Pidhorodetska
+//
+//  Created by Nataliia Pighorodetska on 26.11.19.
+//  Copyright © 2019 Nataliia Pighorodetska. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
